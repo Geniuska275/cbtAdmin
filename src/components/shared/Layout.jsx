3360ef8aae094104c222bc3ce084ec69dd4410a2
Geniuska275 <aigbojie2020@gmail.com>
2024-12-13 00:15:03 +0100

    second

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -7,9 +7,14 @@ function Layout() {
     <div>
       <Header/>
 
-    <div className='flex Flex-row bg-[#E8F4FF] h-[748px] w-screen overflow-hidden' >
+    <div className='flex flex-row bg-[#E8F4FF] h-[748px] w-screen overflow-hidden' >
      <Sidebar/>
      <div>
+      <div className='flex flex-row justify-between items-center mt-3 '>
+        <h1 className='font-bold text-2xl'>Users</h1>
+        <button className='px-3 py-2 bg-[#7EC5FF] text-white font-bold shadow'>Add New Users</button>
+      </div>
+
 
      </div>
       <div>{Outlet}</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div> 
+      <h1 className='text-center'>home page</h1>
       
     </div>
   )
