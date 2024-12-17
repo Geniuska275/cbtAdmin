87a93e9526fb629999669e8114b5c395b7bc1f68
Geniuska275 <aigbojie2020@gmail.com>
2024-12-17 13:46:08 +0100

    good

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaRegUser } from 'react-icons/fa'
 import { TiArrowSortedDown } from "react-icons/ti";
-
+import image from "./cbt.png"
 function Header() {
   return (
     <div className='bg-white py-2 flex  justify-between items-center shadow-xl'>
-      <h1 className='rounded-full w-10 h-10 bg-gray-300 shadow-lg ml-4'>
+      <img src={image} className=' w-[40px] h-[40px] bg-gray-300 shadow-lg ml-4'/>
         
-      </h1>
+      
       <div>
       <div className='flex gap-2 px-4 py-3 items-center '>
                 <FaRegUser/>
